e7d3f3852bde20fc78a56720ac1ca3ce296ecbe4
desiresa <alvarezdaisy16@gmail.com>
2022-03-02 14:59:37 +0000

    Subir el proyecto multimodulo tienda

---
+++ tienda/presentacion/src/test/java/es/iespuerto/tienda/AppTest.java
@@ -0,0 +1,20 @@
+package es.iespuerto.tienda;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
